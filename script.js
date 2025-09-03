5ab950d4a784b1ca32273899f2e491dafa17c8ba
troytechnology <82331479+troytechnology@users.noreply.github.com>
2025-09-03 03:43:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ejemplo simple de interacción
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Gracias por tu mensaje. Te responderé pronto.");
+});
